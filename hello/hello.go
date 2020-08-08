f3e1dac517f9e3672f7991192cfee65aa04ddf62
Lucas Taliberti <lucastaliberti@gmail.com>
2020-08-07 18:53:32 -0700

    Scan method

--- hello/hello.go
+++ hello/hello.go
@@ -12,4 +12,14 @@ func main() {
 
 	fmt.Printf("Hello World, %s, %d\n", name, age)
 	fmt.Println("This is version", version)
+
+	fmt.Println("1- Start monitoring")
+	fmt.Println("2- Show logs")
+	fmt.Println("0- Exit")
+
+	var command int
+	// fmt.Scanf("%d", &command) // Explicit defines the input type
+	fmt.Scan(&command)
+
+	fmt.Println("Chosen command was", command)
 }
